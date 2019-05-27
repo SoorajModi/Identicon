4568caab0db65ec98c61d80edf009c0f62e03e8b
sooraj.modi <sooraj.modi@bell.ca>
2019-05-26 20:21:36 -0400

    Add build_grid() and mirror_row()

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -7,6 +7,28 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_colour
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3)    # splits integer list into 3 part chunks
+    |> mirror_row
+  end
+
+  @doc """
+     Appends the first two elements of an list to the end of the list
+
+  ## Examples
+
+       iex> Identicon.mirror_row([1, 2, 3])
+       [1, 2, 3, 2, 1]
+
+  """
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
   end
 
   # As we receive the image as an arg, we are also pattern matching out of the arguement
