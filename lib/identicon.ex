1f3b3eb277a3176073de4eebd03a12d6055d821d
sooraj.modi <sooraj.modi@bell.ca>
2019-06-03 14:38:36 -0400

    Update dependencies, add draw_image() to generate image from pixel_map and colour

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,6 +10,19 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+  end
+
+  def draw_image(%Identicon.Image{colour: colour, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(colour)
+
+    #iterate over every element in the pixel_map
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
